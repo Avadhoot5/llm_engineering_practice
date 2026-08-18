39f0b3b1e3c93a5a4649f4092fc71ea504a9b140
Avadhoot Bhogil <58785159+Avadhoot5@users.noreply.github.com>
2026-08-18 23:28:37 +0530

    Week 6 Day 1

--- week6/pricer/parser.py
+++ week6/pricer/parser.py
@@ -1,52 +1,91 @@
-from pydantic import BaseModel
-from datasets import Dataset, DatasetDict, load_dataset
-from typing import Optional, Self
+from week6.pricer.items import Item
+import json
+import re
+
+MIN_CHARS = 600
+MIN_PRICE = 0.5
+MAX_PRICE = 999.49
+MAX_TEXT_EACH = 3000
+MAX_TEXT_TOTAL = 4000
+
+REMOVALS = [
+    "Part Number",
+    "Best Sellers Rank",
+    "Batteries Included?",
+    "Batteries Required?",
+    "Item model number",
+]
 
 
-PREFIX = "Price is $"
-QUESTION = "What does this cost to the nearest dollar?"
-
-
-class Item(BaseModel):
+def simplify(text_list) -> str:
     """
-    An Item is a data-point of a Product with a Price
+    Return a simplified string without too much whitespace and limited to MAX_TEXT characters
     """
+    return (
+        str(text_list)
+        .replace("\n", " ")
+        .replace("\r", "")
+        .replace("\t", "")
+        .replace("  ", " ")
+        .strip()[:MAX_TEXT_EACH]
+    )
 
-    title: str
-    category: str
-    price: float
-    full: Optional[str] = None 
-    weight: Optional[float] = None 
-    summary: Optional[str] = None 
-    prompt: Optional[str] = None
-    id:Optional[int] = None
 
-    def make_prompt(self, text: str):
-        self.prompt = f"{QUESTION}\n\n{text}\n\n{PREFIX}{round(self.price)}.00"
+def scrub(title, description, features, details) -> str:
+    """
+    Return a cleansed full string with product numbers and unimportant details removed
+    """
+    for remove in REMOVALS:
+        details.pop(remove, None)
+    result = title + "\n"
+    if description:
+        result += simplify(description) + "\n"
+    if features:
+        result += simplify(features) + "\n"
+    if details:
+        result += json.dumps(details) + "\n"
+    pattern = r"\b(?=[A-Z0-9]{7,}\b)(?=.*[A-Z])(?=.*\d)[A-Z0-9]+\b"
+    return re.sub(pattern, "", result).strip()[:MAX_TEXT_TOTAL]
 
-    def test_prompt(self) -> str:
-        return self.prompt.split(PREFIX)[0] + PREFIX
 
-    def __repr__(self) -> str:
-        return f"<{self.title} = ${self.price}>"
+def get_weight(details):
+    weight_str = details.get("Item Weight")
+    if weight_str:
+        parts = weight_str.split(" ")
+        amount = float(parts[0])
+        unit = parts[1].lower()
+        if unit == "pounds":
+            return amount
+        elif unit == "ounces":
+            return amount / 16
+        elif unit == "grams":
+            return amount / 453.592
+        elif unit == "milligrams":
+            return amount / 453592
+        elif unit == "kilograms":
+            return amount / 0.453592
+        elif unit == "hundredths" and parts[2].lower() == "pounds":
+            return amount / 100
+    return 0
 
-    @staticmethod
-    def push_to_hub(dataset_name: str, train: list[Self], val: list[Self], test: list[Self]):
-        """Push Item lists to HuggingFace Hub"""
-        DatasetDict(
-            {
-                "train": Dataset.from_list([item.model_dump() for item in train]),
-                "validation": Dataset.from_list([item.model_dump() for item in val]),
-                "test": Dataset.from_list([item.model_dump() for item in test])
-            }
-        ).push_to_hub(dataset_name)
 
-    @classmethod
-    def from_hub(cls, dataset_name: str) -> tuple[list[Self], list[Self], list[Self]]:
-        """Load from HuggingFace Hub and reconstruct Items"""
-        ds = load_dataset(dataset_name)
-        return (
-            [cls.model_validate(row) for row in ds["train"]],
-            [cls.model_validate(row) for row in ds["validation"]],
-            [cls.model_validate(row) for row in ds["test"]]
-        )
+def parse(datapoint, category):
+    try:
+        price = float(datapoint["price"])
+    except ValueError:
+        return None
+    if MIN_PRICE <= price <= MAX_PRICE:
+        title = datapoint["title"]
+        description = datapoint["description"]
+        features = datapoint["features"]
+        details = json.loads(datapoint["details"])
+        weight = get_weight(details)
+        full = scrub(title, description, features, details)
+        if len(full) >= MIN_CHARS:
+            return Item(
+                    title=title,
+                    category=category,
+                    price=price,
+                    full=full,
+                    weight=weight,
+                )
